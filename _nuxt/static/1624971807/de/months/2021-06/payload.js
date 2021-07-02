96ac0d022d6af989a78595e5437c1304c7497ae7
Jan Müller <janmueller3698@gmail.com>
2021-07-02 15:33:47 +0000

    Deploying to gh-pages from @ DerYeger/finwa@f96a9e0dfaf5700f178fb1b205ea282f1d226b15 🚀

--- _nuxt/static/1624971807/de/months/2021-06/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/de/months/2021-06", (function(a){return {data:[{slug:a,monthId:a}],fetch:{},mutations:[]}}("2021-06")));
--- _nuxt/static/1624971807/months/2021-06/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/months/2021-06", (function(a){return {data:[{slug:a,monthId:a}],fetch:{},mutations:[]}}("2021-06")));
