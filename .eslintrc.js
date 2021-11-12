bd5ace80e78cab530e28011004b505f639d74a81
Jan Müller <janmueller3698@gmail.com>
2021-11-12 15:08:02 +0100

    chore(eslint): update config

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
   plugins: ['import'],
   rules: {
     'import/no-unresolved': 'error',
+    'vue/multi-word-component-names': 'off',
   },
   settings: {
     'import/parsers': {
