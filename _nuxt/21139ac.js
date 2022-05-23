447c7f5ea426abf0d221c84c186cde5a4d630db8
Jan Müller <janmueller3698@gmail.com>
2022-05-23 03:46:47 +0000

    Deploying to gh-pages from @ DerYeger/finwa@b9d43d43e0b191a73f90f3254bfa50271c9867b2 🚀

---
+++ _nuxt/21139ac.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[33],{544:function(n,e,t){"use strict";t.r(e);var r=t(41),o=Object(r.a)({props:{monthId:{type:String,required:!0}}}),c=t(27),component=Object(c.a)(o,(function(){var n=this,e=n.$createElement;return(n._self._c||e)("span",[n._v(n._s(n.$d(new Date(n.monthId),"no-day")))])}),[],!1,null,null,null);e.default=component.exports}}]);
