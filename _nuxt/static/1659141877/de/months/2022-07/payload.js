f5a19a3c32533595045d8bb0134d279c8b615a49
Jan Müller <janmueller3698@gmail.com>
2022-08-02 05:26:38 +0000

    Deploying to gh-pages from @ DerYeger/finwa@e4494908cda7b6ea072597826fdd0ae861e41dac 🚀

--- _nuxt/static/1659141877/de/months/2022-07/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/de/months/2022-07", (function(a){return {data:[{slug:a,monthId:a}],fetch:{},mutations:[]}}("2022-07")));
---
+++ _nuxt/static/1659417961/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fsettings","\u002Fde\u002Fcategories","\u002Fde\u002Fexpenses","\u002Fde\u002Fincomes","\u002Fde\u002Fmonths","\u002Fde\u002Ffinances","\u002Fde\u002Fsettings","\u002Fcategories","\u002F","\u002Ffinances","\u002Fincomes","\u002Fexpenses","\u002Fmonths","\u002Fde","\u002Fmonths\u002F2022-08","\u002Fde\u002Fmonths\u002F2022-08"]})
