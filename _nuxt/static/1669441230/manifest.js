554e5746946be99f36052080fe50cd8f1f0f3e8a
Jan Müller <janmueller3698@gmail.com>
2022-11-26 05:40:59 +0000

    Deploying to gh-pages from @ DerYeger/finwa@252fee62db9184410427b148008a3f0100fc6026 🚀

---
+++ _nuxt/static/1669441230/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fde\u002Fcategories","\u002Fcategories","\u002Fexpenses","\u002Ffinances","\u002Fincomes","\u002Fmonths","\u002Fsettings","\u002Fde\u002Fexpenses","\u002Fde\u002Fincomes","\u002Fde\u002Ffinances","\u002Fde\u002Fmonths","\u002Fde\u002Fsettings","\u002Fde","\u002Fmonths\u002F2022-11","\u002Fde\u002Fmonths\u002F2022-11"]})
